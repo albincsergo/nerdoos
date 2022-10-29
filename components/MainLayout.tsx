e64c4d15cc385a8c687f70d8b9ffa814b79a860b
Albin Csergő <albincsergo2@gmail.com>
2022-10-29 14:58:36 +0000

    Add files via upload

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,48 +1,51 @@
 import { FC, ReactNode } from "react"
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Box, Center, Spacer, Stack } from '@chakra-ui/react'
-import Navbar from '../components/NavBar'
-import { useWallet } from '@solana/wallet-adapter-react'
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
+import NavBar from "../components/NavBar"
+import { useWallet } from "@solana/wallet-adapter-react"
 
 const MainLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  const { connected } = useWallet()
 
-    const { connected } = useWallet()
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name="The NFT Collection for Buildoors" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Da Nerdoos</title>
-                <meta name="The NFT collection for nerds, geniuses, and trustfund babies" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+      <Box
+        w="full"
+        h="calc(100vh)"
+        bgImage={connected ? "" : "url(/home-background.svg)"}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="calc(100vh)" justify="center">
+          <NavBar />
 
-            <Box
-                w='full'
-                h='calc(100vh)'
-                bgImage={connected ? "" : "url(/home-background.svg)"}
-                backgroundPosition="center"
-            >
-                <Stack w="full" h="calc(100vh)" justify="center">
-                    <Navbar />
+          <Spacer />
 
-                    <Spacer />
+          <Center>{children}</Center>
 
-                    <Center>{children}</Center>
+          <Spacer />
 
-                    <Spacer />
-
-                    <Center>
-                        <Box marginBottom={4} color="white">
-                            <a href='https://twitter.com/albincsergo' target='_blank' rel='noopener noreferrer'>
-                                Engineered by @albincsergo
-                            </a>
-                        </Box>
-                    </Center>
-                </Stack>
+          <Center>
+            <Box marginBottom={4} color="white">
+              <a
+                href="https://twitter.com/_buildspace"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                build with @_buildspace
+              </a>
             </Box>
-        </div>
-    )
+          </Center>
+        </Stack>
+      </Box>
+    </div>
+  )
 }
 
 export default MainLayout
